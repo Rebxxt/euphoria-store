cd87a96bf0446d1597528fd631256dda0223e75c
Rebxxt <allrebxxt@gmail.com>
2023-09-13 02:16:09 +0300

    commit skipped

---
+++ src/app/pages/auth-old/auth-old.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-old',
+  templateUrl: './auth-old.component.html',
+  styleUrls: ['./auth-old.component.css']
+})
+export class AuthOldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

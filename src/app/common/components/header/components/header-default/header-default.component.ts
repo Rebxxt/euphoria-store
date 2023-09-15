036ff4a1e9cf9cf7c30a56536d1c09f1a1aa0c58
Rebxxt <allrebxxt@gmail.com>
2023-09-16 01:21:09 +0300

    #Mod: Update default header

--- src/app/common/components/header/components/header-default/header-default.component.ts
+++ src/app/common/components/header/components/header-default/header-default.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Event, NavigationEnd, Router} from "@angular/router";
+import {filter} from "rxjs";
+import {AuthPage} from "../../../../types/auth";
 
 @Component({
   selector: 'app-header-default',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderDefaultComponent implements OnInit {
 
-  constructor() { }
+  public selectedRoute: 'sign-in' | 'sign-up' | null = null;
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.setRoute(this.router.url)
+    this.router.events.pipe(filter(v => v instanceof NavigationEnd)).subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        this.setRoute(event.url)
+      }
+    })
   }
 
+  private setRoute(route: string) {
+    if (route.includes(AuthPage.SIGN_IN)) {
+      this.selectedRoute = 'sign-in'
+    } else if (route.includes(AuthPage.SIGN_UP)) {
+      this.selectedRoute = 'sign-up'
+    } else {
+      this.selectedRoute = null;
+    }
+  }
 }
